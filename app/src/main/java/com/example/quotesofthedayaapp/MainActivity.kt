42baf037a801b62a48fdf1fc06003daa353caf74
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-02-18 18:04:55 +0100

    Some ui

--- app/src/main/java/com/example/quotesofthedayaapp/MainActivity.kt
+++ app/src/main/java/com/example/quotesofthedayaapp/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.quotesofthedayaapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.quotesofthedayaapp.ui.theme.Background
 import com.example.quotesofthedayaapp.ui.theme.QuotesOfTheDayaAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,27 +29,28 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuotesOfTheDayaAppTheme {
-        Greeting("Android")
+fun MainScreen(){
+    Box(modifier = Modifier.fillMaxSize()){
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = "Quote of the day",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Black,
+                modifier = Modifier.padding(bottom = 50.dp)
+            )
+        }
     }
 }
